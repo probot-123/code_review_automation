d51a0fd86daa710ccb21f61f269e5c60d9be4734
Yash <your_email@example.com>
2025-05-26 20:09:42 +0530

    using 1 space indenting in test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 class StudentRecord:
     def __init__(self, StudentName, RollNumber, TotalMarks):
-        self.StudentName = StudentName
-        self.RollNumber = RollNumber
-        self.TotalMarks = TotalMarks
+     self.StudentName = StudentName
+     self.RollNumber = RollNumber
+     self.TotalMarks = TotalMarks
 
     def DisplayDetails(self):
         print(f"Name: {self.StudentName}")
